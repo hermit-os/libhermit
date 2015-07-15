f2449947f81875187c7ed36d6a01eefe232a9448
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2015-07-15 10:05:09 +0200

    redesign of system call handling
    - use a table to determine the address of the system call
    - fix numbering of the system calls

--- hermit/newlib/src/libgloss/hermit/syscall.h
+++ hermit/newlib/src/libgloss/hermit/syscall.h
@@ -37,44 +37,44 @@ extern "C" {
 #define __NR_open		2
 #define __NR_close		3
 #define __NR_read		4
-#define __NR_lseek		6
-#define __NR_unlink		7
-#define __NR_getpid		8
-#define __NR_kill		9
-#define __NR_fstat		10
-#define __NR_sbrk		11
-#define __NR_fork		12
-#define __NR_wait		13
-#define __NR_execve		14
-#define __NR_times		15
-#define __NR_accept		16
-#define __NR_bind		17
-#define __NR_closesocket	18
-#define __NR_connect		19
-#define __NR_listen		20
-#define __NR_recv		21
-#define __NR_send		22
-#define __NR_socket		23
-#define __NR_getsockopt		24
-#define __NR_setsockopt		25
-#define __NR_gethostbyname	26
-#define __NR_sendto		27
-#define __NR_recvfrom		28
-#define __NR_select		29
-#define __NR_stat		30
-#define __NR_dup		31
-#define __NR_dup2		32
+#define __NR_lseek		5
+#define __NR_unlink		6
+#define __NR_getpid		7
+#define __NR_kill		8
+#define __NR_fstat		9
+#define __NR_sbrk		10
+#define __NR_fork		11
+#define __NR_wait		12
+#define __NR_execve		13
+#define __NR_times		14
+#define __NR_accept		15
+#define __NR_bind		16
+#define __NR_closesocket	17
+#define __NR_connect		18
+#define __NR_listen		19
+#define __NR_recv		20
+#define __NR_send		21
+#define __NR_socket		22
+#define __NR_getsockopt		23
+#define __NR_setsockopt		24
+#define __NR_gethostbyname	25
+#define __NR_sendto		26
+#define __NR_recvfrom		27
+#define __NR_select		28
+#define __NR_stat		29
+#define __NR_dup		30
+#define __NR_dup2		31
 
 inline static long
 syscall(int nr, unsigned long arg0, unsigned long arg1, unsigned long arg2)
 {
 	long res;
 
-	// note: syscall stores the return address in rcx
-	asm volatile ("mov %4, %%r10; syscall"
+	// note: syscall stores the return address in rcx and rflags in r11
+	asm volatile ("syscall"
 		: "=a" (res)
-		: "D" (nr), "S" (arg0), "d" (arg1), "r" (arg2)
-		: "memory", "%rcx", "%r10", "%r11");
+		: "a" (nr), "D" (arg0), "S" (arg1), "d" (arg2)
+		: "memory", "%rcx", "%r11");
 
 	return res;
 }
