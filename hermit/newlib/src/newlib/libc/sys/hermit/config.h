1d8e287058e4b61c3a52c31fa64eb5f5dd5f598b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2015-08-07 16:06:00 +0200

    move hermit support to the right location
    => to newlib

---
+++ hermit/newlib/src/newlib/libc/sys/hermit/config.h
@@ -0,0 +1,23 @@
+/* Name of package.  */
+#define PACKAGE		hermitGloss
+
+/* Version of package.  */
+#define VERSION		0.1
+
+/* Using ELF format */
+#define HAVE_ELF			1
+
+/* Using GNU LD */
+#define HAVE_GNU_LD			1
+
+/* .previous directive allowed */
+#define HAVE_ASM_PREVIOUS_DIRECTIVE	1
+
+/* .pushsection/.popsection directives allowed */
+#define HAVE_ASM_POPSECTION_DIRECTIVE	1
+
+/* support for section attributes */
+#define HAVE_SECTION_ATTRIBUTES		1
+
+/* symbol prefix */
+#define __SYMBOL_PREFIX			""
