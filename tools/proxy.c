0140c3ca562377b8fac34b387711e9e01cc3ebc9
Jens Breitbart <jbreitbart@gmail.com>
2017-09-05 18:31:47 +0200

    Reorder includes.
    Sorry automatic code formating.

--- tools/proxy.c
+++ tools/proxy.c
@@ -27,25 +27,24 @@
 
 #define _GNU_SOURCE
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
 #include <arpa/inet.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <linux/tcp.h>
+#include <net/if.h>
 #include <netinet/in.h>
 #include <sched.h>
 #include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/inotify.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
+#include <unistd.h>
 
 #include "proxy.h"
 
