64c328d842b9fc26b4f520c4d3b350611a275d47
Beichen Liu <bcliu430@vt.edu>
2017-11-06 00:04:38 -0500

    write own func

--- usr/socket/run.sh
+++ usr/socket/run.sh
@@ -2,10 +2,10 @@
 
 set -eu
 
-rm -rf ./build || true
-mkdir build
-cd build && cmake .. && make
+##rm -rf ./build || true
+##mkdir build
+##cd build && cmake .. && make
 
-gcc -o client ../client.c
+##gcc -o client ../client.c
 
-## HERMIT_ISLE=qemu HERMIT_APP_PORT=22000 /opt/hermit/bin/proxy ./build/server
+ HERMIT_ISLE=qemu HERMIT_APP_PORT=22000 /opt/hermit/bin/proxy ./build/server
